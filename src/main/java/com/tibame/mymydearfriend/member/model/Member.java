6a5b47fe4b24feb675fd552a93b5c0dc783884ae
Hank <hank861202@gmail.com>
2024-04-01 15:29:57 +0800

    create member setter getter

---
+++ src/main/java/com/tibame/mymydearfriend/member/model/Member.java
@@ -0,0 +1,23 @@
+package com.tibame.mymydearfriend.member.model;
+
+public class Member {
+	private Integer memNO;
+	private String memName;
+
+	public Integer getMemNO() {
+		return memNO;
+	}
+
+	public void setMemNO(Integer memNO) {
+		this.memNO = memNO;
+	}
+
+	public String getMemName() {
+		return memName;
+	}
+
+	public void setMemName(String memName) {
+		this.memName = memName;
+	}
+
+}
