36443165201b2534a61c4d9fedea8c383103f89f
karta0128 <128357502+karta0128@users.noreply.github.com>
2024-04-01 14:50:46 +0800

    HelloWorld.java

--- src/main/java/com/tibame/mymydearfriend/HelloWorld.java
+++ src/main/java/com/tibame/mymydearfriend/HelloWorld.java
@@ -3,6 +3,6 @@ package com.tibame.mymydearfriend;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello");
-		System.out.println("Wecome to BUY ~ SELL My Dear Friend!")
+		System.out.println("BUY ~ SELL My Dear Friend!")
 	}
 }
